1ef3018c8a6a2a6df8011e69d871e09c8f3ab80f
Tashdik <s2110477153@ru.ac.bd>
2024-05-10 02:59:52 +0600

    initial set up and authentication complete

---
+++ src/routes/AllRoutes.jsx
@@ -0,0 +1,30 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../pages/MainLayout";
+import Home from "../pages/home/Home";
+import Login from "../pages/login/Login";
+import Register from "../pages/register/Register";
+
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout />,
+        children:[
+            {
+                path: '/',
+                element: <Home />
+            },
+            {
+                path: '/login',
+                element: <Login />
+            },
+            {
+                path: '/register',
+                element: <Register />
+            },
+        ]
+    },
+]);
+
+export default router;
