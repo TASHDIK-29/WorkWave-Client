6bcd5425a9e4cea217a5a4fe6715c9e99f8a5077
Tashdik <s2110477153@ru.ac.bd>
2024-05-13 00:22:57 +0600

    successfully showed review instently from database

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "./Banner";
 import FeaturedPost from "./FeaturedPost";
+import Review from "./Review";
 
 const Home = () => {
     return (
         <div>
             <Banner />
             <FeaturedPost />
+            <Review />
         </div>
     );
 };
---
+++ src/pages/home/Review.jsx
@@ -0,0 +1,152 @@
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../provider/AuthProvider";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+
+const Review = () => {
+
+    const { user } = useContext(AuthContext);
+
+    const [reviews, setReviews] = useState([]);
+
+
+    useEffect(() => {
+
+        getReviews();
+    }, [user])
+
+
+    const getReviews = () => {
+        fetch('http://localhost:5000/review')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setReviews(data);
+            })
+    }
+
+    const handelSubmit = e => {
+        e.preventDefault();
+
+        if (!user) {
+            return alert('You Have to login First');
+        }
+
+        const review = e.target.review.value;
+        const data = {
+            author: user?.displayName,
+            photo: user?.photoURL,
+            review
+        }
+        // setReview(data);
+
+
+        fetch('http://localhost:5000/review', {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.insertedId) {
+                    alert('Successfully Posted')
+                    getReviews();
+                }
+            })
+    }
+
+    // console.log(review);
+
+
+    // useEffect(() => {
+    //     fetch('http://localhost:5000/review', {
+    //         method: "POST",
+    //         headers: {
+    //             "content-type": "application/json"
+    //         },
+    //         body: JSON.stringify(review)
+    //     })
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             console.log(data);
+    //             if (data.insertedId) {
+    //                 alert('Successfully Posted')
+    //             }
+    //         })
+    // }, [review])
+
+    return (
+        <div className="my-8">
+            <h1 className="text-3xl font-bold text-center">We Care Your Thoughts</h1>
+            <div className="flex">
+                <div className="w-1/2 border">
+                    <div>
+                        <Swiper
+                            spaceBetween={30}
+                            centeredSlides={true}
+                            autoplay={{
+                                delay: 2500,
+                                disableOnInteraction: false,
+                            }}
+                            // loop={true}
+
+                            
+                            // pagination={{
+                            //     clickable: true,
+                            // }}
+                            // navigation={true}
+                            modules={[Autoplay]}
+                            className="mySwiper"
+                        >
+                            {
+                                reviews?.map(review => <SwiperSlide key={review._id}>
+                                    <div className="w-full max-w-md px-8 py-4 mt-16 bg-white rounded-lg shadow-lg dark:bg-gray-800">
+                                        <div className="flex justify-center -mt-16 md:justify-end">
+                                            <img className="object-cover w-20 h-20 border-2 border-blue-500 rounded-full dark:border-blue-400" alt="Testimonial avatar" src={review.photo} />
+                                        </div>
+
+                                        <h2 className="mt-2 text-xl font-semibold text-gray-800 dark:text-white md:mt-0">Thoughts</h2>
+
+                                        <p className="mt-2 text-sm text-gray-600 dark:text-gray-200">{review.review}</p>
+
+                                        <div className="flex justify-end mt-4">
+                                            <a href="#" className="text-lg font-medium text-blue-600 dark:text-blue-300" tabIndex="0" role="link">{review.author}</a>
+                                        </div>
+                                    </div>
+                                </SwiperSlide>)
+                            }
+
+                        </Swiper>
+                    </div>
+                </div>
+                <div className="w-1/2 border">
+                    <form onSubmit={handelSubmit} className="w-2/3">
+                        <label htmlFor="Description" className="block text-sm text-gray-500 dark:text-gray-300">Put tour Thoughts</label>
+
+                        <textarea
+                            placeholder="lorem..."
+                            name="review"
+                            className="block mt-2 w-full placeholder-gray-400/70 dark:placeholder-gray-500 rounded-lg border border-gray-200 bg-white px-4 h-32 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 d dark:text-gray-300 dark:focus:border-blue-300"
+                        ></textarea>
+                        <input className="border p-2 border-red-600" type="submit" value="Submit" />
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Review;
